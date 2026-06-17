1964bd1ef1702ae2c732cfed7e8002effcfee003
Terence Montecillo <4hoursleft@gmail.com>
2026-06-17 17:11:41 +0800

    adad

--- src/api/index.js
+++ src/api/index.js
@@ -3,6 +3,5 @@ dotenv.config({ path: '.env.local' })
 
 export default function handler(req, res) {
     console.log("Hey you found me!")
-    console.log(process.env.DISCORD_WEBHOOK_URL)
     res.status(200).json({ message: "ok" })
 }
